abcc7bc4b5b23fc07ab475e07d6f45ddf64144f2
GitQuintin <qlemek@student.fitchburgstate.edu>
2026-03-16 20:58:04 -0400

    Add files via upload

--- mathmatch.sql
+++ mathmatch.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 14, 2026 at 02:56 AM
+-- Generation Time: Mar 16, 2026 at 08:16 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -24,6 +24,19 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `answers_given`
+--
+
+CREATE TABLE `answers_given` (
+  `answers_given_ID` int(11) NOT NULL,
+  `userID` int(100) NOT NULL,
+  `question_ID` int(11) NOT NULL,
+  `answer` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `classes`
 --
 
@@ -31,6 +44,31 @@ CREATE TABLE `classes` (
   `classID` int(11) NOT NULL,
   `class_name` varchar(80) NOT NULL,
   `description` varchar(256) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `qa_ag`
+--
+
+CREATE TABLE `qa_ag` (
+  `qa_ag_ID` int(100) NOT NULL,
+  `questions_asked_ID` int(11) NOT NULL,
+  `answers_given_ID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `questions_asked`
+--
+
+CREATE TABLE `questions_asked` (
+  `questions_asked_ID` int(11) NOT NULL,
+  `userID` int(100) NOT NULL,
+  `answer_ID` int(11) NOT NULL,
+  `question` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -47,6 +85,29 @@ CREATE TABLE `session` (
   `TutorID` int(11) NOT NULL,
   `StudentID` int(11) NOT NULL,
   `is_scheduled` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tutors`
+--
+
+CREATE TABLE `tutors` (
+  `tutorID` int(100) NOT NULL,
+  `userID` int(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tutors_classes`
+--
+
+CREATE TABLE `tutors_classes` (
+  `tutors_classes` int(100) NOT NULL,
+  `tutorID` int(100) NOT NULL,
+  `classID` int(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -73,10 +134,32 @@ CREATE TABLE `users` (
 --
 
 --
+-- Indexes for table `answers_given`
+--
+ALTER TABLE `answers_given`
+  ADD PRIMARY KEY (`answers_given_ID`),
+  ADD KEY `userID` (`userID`);
+
+--
 -- Indexes for table `classes`
 --
 ALTER TABLE `classes`
   ADD PRIMARY KEY (`classID`);
+
+--
+-- Indexes for table `qa_ag`
+--
+ALTER TABLE `qa_ag`
+  ADD PRIMARY KEY (`qa_ag_ID`),
+  ADD KEY `questions_asked_ID` (`questions_asked_ID`),
+  ADD KEY `answers_given_ID` (`answers_given_ID`);
+
+--
+-- Indexes for table `questions_asked`
+--
+ALTER TABLE `questions_asked`
+  ADD PRIMARY KEY (`questions_asked_ID`),
+  ADD KEY `userID` (`userID`);
 
 --
 -- Indexes for table `session`
@@ -86,6 +169,21 @@ ALTER TABLE `session`
   ADD KEY `ClassID` (`ClassID`),
   ADD KEY `TutorID` (`TutorID`),
   ADD KEY `StudentID` (`StudentID`);
+
+--
+-- Indexes for table `tutors`
+--
+ALTER TABLE `tutors`
+  ADD PRIMARY KEY (`tutorID`),
+  ADD KEY `userID` (`userID`);
+
+--
+-- Indexes for table `tutors_classes`
+--
+ALTER TABLE `tutors_classes`
+  ADD PRIMARY KEY (`tutors_classes`),
+  ADD KEY `classID` (`classID`),
+  ADD KEY `tutorID` (`tutorID`);
 
 --
 -- Indexes for table `users`
@@ -101,16 +199,46 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `answers_given`
+--
+ALTER TABLE `answers_given`
+  MODIFY `answers_given_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `classes`
 --
 ALTER TABLE `classes`
   MODIFY `classID` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `qa_ag`
+--
+ALTER TABLE `qa_ag`
+  MODIFY `qa_ag_ID` int(100) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `questions_asked`
+--
+ALTER TABLE `questions_asked`
+  MODIFY `questions_asked_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `session`
 --
 ALTER TABLE `session`
   MODIFY `session_ID` int(100) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tutors`
+--
+ALTER TABLE `tutors`
+  MODIFY `tutorID` int(100) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tutors_classes`
+--
+ALTER TABLE `tutors_classes`
+  MODIFY `tutors_classes` int(100) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `users`
@@ -123,12 +251,44 @@ ALTER TABLE `users`
 --
 
 --
+-- Constraints for table `answers_given`
+--
+ALTER TABLE `answers_given`
+  ADD CONSTRAINT `answers_given_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `users` (`userID`);
+
+--
+-- Constraints for table `qa_ag`
+--
+ALTER TABLE `qa_ag`
+  ADD CONSTRAINT `qa_ag_ibfk_1` FOREIGN KEY (`questions_asked_ID`) REFERENCES `questions_asked` (`questions_asked_ID`),
+  ADD CONSTRAINT `qa_ag_ibfk_2` FOREIGN KEY (`answers_given_ID`) REFERENCES `answers_given` (`answers_given_ID`);
+
+--
+-- Constraints for table `questions_asked`
+--
+ALTER TABLE `questions_asked`
+  ADD CONSTRAINT `questions_asked_ibfk_1` FOREIGN KEY (`userID`) REFERENCES `users` (`userID`);
+
+--
 -- Constraints for table `session`
 --
 ALTER TABLE `session`
   ADD CONSTRAINT `session_ibfk_1` FOREIGN KEY (`ClassID`) REFERENCES `classes` (`classID`),
   ADD CONSTRAINT `session_ibfk_2` FOREIGN KEY (`TutorID`) REFERENCES `users` (`userID`),
   ADD CONSTRAINT `session_ibfk_3` FOREIGN KEY (`StudentID`) REFERENCES `users` (`userID`);
+
+--
+-- Constraints for table `tutors`
+--
+ALTER TABLE `tutors`
+  ADD CONSTRAINT `userID` FOREIGN KEY (`userID`) REFERENCES `users` (`userID`);
+
+--
+-- Constraints for table `tutors_classes`
+--
+ALTER TABLE `tutors_classes`
+  ADD CONSTRAINT `classID` FOREIGN KEY (`classID`) REFERENCES `classes` (`classID`),
+  ADD CONSTRAINT `tutorID` FOREIGN KEY (`tutorID`) REFERENCES `tutors` (`tutorID`);
 
 --
 -- Constraints for table `users`
